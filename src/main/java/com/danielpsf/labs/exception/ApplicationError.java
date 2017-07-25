4f215e61b4bdedbe2be1907303bea1ae041116db
Daniel Fernandes <3307367+danielpsf@users.noreply.github.com>
2017-07-25 16:58:53 -0300

    [#5] - Adding custom error handling

---
+++ src/main/java/com/danielpsf/labs/exception/ApplicationError.java
@@ -0,0 +1,37 @@
+package com.danielpsf.labs.exception;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ApplicationError {
+
+    private String message;
+    private int statusCode;
+
+    public ApplicationError() {
+    }
+
+    public ApplicationError(String message, int statusCode) {
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+}
