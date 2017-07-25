9640f5452876288412e45637d82a8f3620dcf6b5
Daniel Fernandes <3307367+danielpsf@users.noreply.github.com>
2017-07-25 14:23:41 -0300

    Implementing #1 - simple bean validation

---
+++ src/main/java/com/danielpsf/labs/message/MessageController.java
@@ -0,0 +1,44 @@
+package com.danielpsf.labs.message;
+
+import java.net.URISyntaxException;
+
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.danielpsf.labs.BaseController;
+
+@Path("/message")
+public class MessageController extends BaseController {
+
+    MessageService messageService = null;
+
+    public MessageController() {
+        messageService = new MessageService();
+    }
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response get(@PathParam("id") Integer id) {
+        return Response.ok(messageService.get(id))
+                       .build();
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response save(@Valid final Message message) throws URISyntaxException {
+        Message savedMessage = messageService.save(message);
+        return Response.created(getCreatedResourceUri("message", savedMessage.getId()))
+                       .entity(savedMessage)
+                       .build();
+    }
+}
